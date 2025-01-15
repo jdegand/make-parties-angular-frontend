6528ccbc2a75f2dc261bd89632ca2d3123e428e2
jdegand <70610011+jdegand@users.noreply.github.com>
2025-01-14 23:26:43 -0500

    fix: update rsvp service typings

--- src/app/service/rsvp-service.service.ts
+++ src/app/service/rsvp-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RsvpObj } from '../interfaces/RsvpObj';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,11 @@ export class RsvpService {
 
   constructor(private http: HttpClient) { }
 
-  postRsvp(eventId: string | null | undefined, payload: any){
+  postRsvp(eventId: string, payload: Partial<RsvpObj>){
     return this.http.post(`http://localhost:8080/events/${eventId}/rsvps`, payload);
   }
 
-  deleteRsvp(eventId: string | null | undefined, rsvpId: string | null | undefined){
+  deleteRsvp(eventId: string, rsvpId: string){
     return this.http.delete(`http://localhost:8080/events/${eventId}/rsvps/${rsvpId}`);
   }
 }
